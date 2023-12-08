008ead1ea3211c0ac2dc07ba57098347040a6366
Graham Williamson <526612+argfrot@users.noreply.github.com>
2023-12-08 22:41:27 +0000

    Day 2, part 1

---
+++ day_2/main.py
@@ -0,0 +1,40 @@
+def get_example_input():
+    with open('day_2/example.txt', 'r') as f:
+        data = [parse(line) for line in f.readlines()]
+    return data
+
+def get_input():
+    with open('day_2/input.txt', 'r') as f:
+        data = [parse(line) for line in f.readlines()]
+    return data
+
+def parse(line):
+    a = line.split(':')
+    game = int(a[0].split()[1])
+    draws = a[1].split(';')
+    reveals = []
+    for draw in draws:
+        red, green, blue = 0, 0, 0
+        balls = draw.split(',')
+        for ball in balls:
+            n, colour = map(str.strip, ball.split())
+            if colour == 'red':
+                red = int(n)
+            elif colour == 'green':
+                green = int(n)
+            elif colour == 'blue':
+                blue = int(n)
+            else:
+                raise 'Unknown colour: %s' % colour
+        reveals.append((red,green,blue))
+    return (game, reveals)
+
+def games_below_max(data, max_red, max_green, max_blue):
+    for game, reveals in data:
+        r,g,b = list(map(max, zip(*reveals)))
+        if r<=max_red and g<=max_green and b<=max_blue:
+            yield game
+
+if __name__ == '__main__':
+    data = get_input()
+    print(sum(games_below_max(data, 12, 13, 14)))
